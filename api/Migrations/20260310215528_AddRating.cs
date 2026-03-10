c033a30548fc0892678fd85daa0974c4c5acc8ba
AzzamAlfulayw <azzamalfulayw@gmail.com>
2026-03-11 00:58:35 +0300

    made some cahnges to repositories and dtos

---
+++ api/Migrations/20260310215528_AddRating.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dc67dfc4-29b3-40de-940f-b9661275e65a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f869cb84-8e00-4234-b82c-80ba02269ac2");
+
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StockId = table.Column<int>(type: "int", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ratings_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Stocks_StockId",
+                        column: x => x.StockId,
+                        principalTable: "Stocks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3580b111-fedb-4ba4-a769-9cf37c7d7554", "7dbe374c-06b9-4a1f-aa6d-095019e64bab", "User", "USER" },
+                    { "7b01186c-1192-4198-9e39-314c4fe1fe92", "91732f6f-d44b-4012-9738-be299f669d25", "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_AppUserId",
+                table: "Ratings",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_StockId",
+                table: "Ratings",
+                column: "StockId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ratings");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3580b111-fedb-4ba4-a769-9cf37c7d7554");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7b01186c-1192-4198-9e39-314c4fe1fe92");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "dc67dfc4-29b3-40de-940f-b9661275e65a", "0ebc8bc4-3a5f-4288-a50a-fd7a76b4fcbf", "User", "USER" },
+                    { "f869cb84-8e00-4234-b82c-80ba02269ac2", "e5bedc7c-41d0-4e77-8abd-e79c30541d42", "Admin", "ADMIN" }
+                });
+        }
+    }
+}
